72a7717e765b400263396ad916a7f5ec66b9d376
Ramya Mol R <103188616+ramya45r@users.noreply.github.com>
2022-09-20 19:19:15 +0530

    Add files via upload

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express =require('express')
+const router = express.Router()
+const {findOneuser,findUsers,deleteUser,editUser
+} =require('../controllers/userController')
+const {protect} = require('../middleware/authMiddleware')
+
+router.get('/',protect,findUsers)
+router.get('/finduser/:id',protect,findOneuser)
+router.delete('/deleteuser/:id',protect,deleteUser)
+router.put('/edituser/:id',protect,editUser)
+
+
+module.exports =router;
